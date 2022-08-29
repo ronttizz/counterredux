5a36b09d35ece04eb3f6a3470b793c135213cc7f
ronttizz <toni.nissinen@gmail.com>
2022-08-29 14:43:54 +0300

    initialState to 0, increment now accepts second parameter

--- src/features/counter/CounterSlice.js
+++ src/features/counter/CounterSlice.js
@@ -3,11 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: "counter",
   initialState: {
-    count: 100,
+    count: 0,
   },
   reducers: {
-    increment: (state) => {
-      state.count += 1; // count refers to initialState count
+    increment: (state, action) => {
+      state.count += action.payload; // count refers to initialState count
     },
   },
 });
